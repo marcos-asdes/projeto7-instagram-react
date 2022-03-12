b2a9b0be58c4134a4fb9b1a042d9313468442d08
marcos-asdes <marco.engc@poli.ufrj.br>
2022-03-12 19:09:22 -0300

    basic html structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Top from './Top'
+import StoriesAndFeed from './StoriesAndFeed'
+import SideBar from './SideBar'
+
+export default function App() {
+    return (
+        <section>
+            <Top />
+            <StoriesAndFeed />
+            <SideBar />
+        </section>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import ReactDOM from 'react-dom';
-
-function App() {
-    return (
-        <section>
-            <Top />
-            <Stories-And-Feed />
-            <Side-Bar />
-        </section>
-    )
-}
+import App from './App.js';
 
 ReactDOM.render(<App />, document.querySelector(".root"));
