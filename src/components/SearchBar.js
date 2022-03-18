8993aea1ac0fc65d633d85732f5267ccc0fee90a
pedro imbrech <elprogramadorpedro@gmail.com>
2022-03-18 16:41:52 -0300

    rev

--- src/components/SearchBar.js
+++
@@ -1,9 +0,0 @@
-export default function SearchBar() {
-    return (
-        <div className='SearchBar'>
-            <form>
-                <input type="text" className="search" placeholder="Pesquisar"></input>
-            </form>
-        </div>
-    )
-}
